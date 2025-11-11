05ba2cdb8d51396371c27056acdf70c663af41df
Pedro Henrique Levorato <pedro.levorato@weisul.com.br>
2025-11-11 11:36:50 -0300

    Configurar next.config.js para produÃ§Ã£o com basePath condicional

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // Configurações para GitHub Pages (descomente para build de produção)
-  // basePath: '/siteprintbag',
-  // output: 'export',
+  // Configurações para GitHub Pages
+  basePath: process.env.NODE_ENV === 'production' ? '/siteprintbag' : '',
+  output: 'export',
   images: {
     unoptimized: true,
   },
